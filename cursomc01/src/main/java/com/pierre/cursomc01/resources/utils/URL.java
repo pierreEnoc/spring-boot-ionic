85dbb2f8da0ce63cc3e67a16f9346d133d4fdd2e
pierreEnoc <pierre_enoc@yahoo.fr>
2019-10-11 09:15:55 -0300

    Busca de produtos por nome e categorias

--- cursomc01/src/main/java/com/pierre/cursomc01/resources/utils/URL.java
+++ cursomc01/src/main/java/com/pierre/cursomc01/resources/utils/URL.java
@@ -7,13 +7,14 @@ import java.util.List;
 
 public class URL {
 
-public static String decodeParam(String s) {
-	try {
-		return URLDecoder.decode(s,"UTF-8");
-	} catch (UnsupportedEncodingException e) {
-		return "";
-	}
-}
+	public static String decodeParam(String s) {
+		try {
+			return URLDecoder.decode(s, "UTF-8");
+		} 
+		catch (UnsupportedEncodingException e) {
+			return "";
+		}
+	}	
 
   public static List<Integer> decodeIntList(String s){
 	  String[] vet = s.split(",");
