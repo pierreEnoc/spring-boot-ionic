c05c9b13ab45097de3e5c7bd3a2cd75b55c0f23d
pierreEnoc <pierre_enoc@yahoo.fr>
2019-10-11 09:31:39 -0300

    sub total e total produto

--- cursomc01/src/main/java/com/pierre/cursomc01/domain/Pedido.java
+++ cursomc01/src/main/java/com/pierre/cursomc01/domain/Pedido.java
@@ -57,8 +57,15 @@ public class Pedido implements Serializable {
 			this.enderecoDeEntrega = enderecoDeEntrega;
 		}
 
-
-		public Integer getId() {
+public double getValorTotal() {
+	double soma =0.0;
+	for(ItemPedido ip : itens) {
+		soma = soma + ip.getSubTotal();
+	}
+	return soma;
+   }
+		
+public Integer getId() {
 			return id;
 		}
 
