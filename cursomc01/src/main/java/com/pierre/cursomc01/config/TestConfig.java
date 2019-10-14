ed2499ce48ec1419e021a30b65259a681751e26c
pierreEnoc <pierre_enoc@yahoo.fr>
2019-10-13 21:10:07 -0300

    criando profile de test

---
+++ cursomc01/src/main/java/com/pierre/cursomc01/config/TestConfig.java
@@ -0,0 +1,25 @@
+package com.pierre.cursomc01.config;
+
+import java.text.ParseException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.pierre.cursomc01.services.DBService;
+
+@Configuration
+@Profile("test")
+public class TestConfig {
+	@Autowired
+	private DBService dbService;
+		
+	
+	@Bean
+	public boolean instantiateDatabase() throws ParseException {
+		dbService.instatiateTestDatabase();	
+		return true;
+	}
+
+}
